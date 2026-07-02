70db31cbcb5802256873ad65ca7fe46ccf651d6e
taerin <taerin@qualisports.kr>
2026-07-02 10:57:27 +0900

    폰트 패밀리 웹폰트로 변경

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5ccb27be'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qpuecaevdps"
+    "revision": "0.ralr98ik0q8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
