0d7269d2c67db04188393404d463b5ecae50a7e2
taerin <taerin@qualisports.kr>
2026-07-21 22:19:29 +0900

    full-guides 분기에서는 캐싱적용 비활성화

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       manifest: false, // 단순히 캐싱 목적이므로 앱 설치 관련 매니페스트는 비활성화
       devOptions: {
-        enabled: true, // 로컬 개발 환경에서도 테스트 가능하도록 설정
+        enabled: false, // 로컬 개발 환경에서는 MD 파일 실시간 반영을 위해 캐싱 끄기
       },
       workbox: {
         // 기본 캐싱 설정만 사용 (네이티브 브라우저 캐시 활용)
